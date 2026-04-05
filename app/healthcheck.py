565a284a72c1e0e437006c83aace04305fd2e95c
Guiilario <164490533+Guiilario@users.noreply.github.com>
2026-04-05 03:53:04 -0300

    Delete app directory

--- app/healthcheck.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-healthcheck_bp = Blueprint('healthcheck', __name__)
-
-@healthcheck_bp.route('/health')
-def health_check():
-    """Health check endpoint for Docker"""
-    return "OK", 200 
